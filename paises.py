d6aecef608c42efddecace179cc1abedb635fe50
Martin Vazquez <martin@linisco.com.ar>
2025-10-14 16:10:44 -0300

    filtrado por continente

--- paises.py
+++ paises.py
@@ -10,8 +10,14 @@ def filtrar_por_continente(paises: list, continente: str) -> list:
     """filtra los paises por continente
     recibe: la lista de paises, y el continente por el que desea filtrar
     retorna: una lista nueva con los paises que tienen el continente recibido"""
+
+    paises_por_continente = []
+
+    for pais in paises:
+        if continente.lower() == pais['continente'].lower():
+            paises_por_continente.append(pais)
     
-    return []
+    return paises_por_continente
 
 
 def filtrar_rango_poblacion(paises: list, poblacion_minima: int, poblacion_maxima: int) -> list:
